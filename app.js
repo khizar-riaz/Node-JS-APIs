e1e0dff665200dfa2dbd04593d631e665c3ee495
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-26 12:58:34 +0200

    delet user resources in Database

--- app.js
+++ app.js
@@ -7,39 +7,17 @@ const dbConnect = require("./db/dbConnect");
 const User = require("./db/userModel");
 
 dbConnect();
-//inserting new user
-const newUser = new User({
-  email: "khan22@example2.com",
-  password: "0922220078601",
-});
-newUser
-  .save()
-  .then(() => {
-    console.log("User Inserted Successfully");
-  })
-  .catch((error) => {
-    console.log("Error inserting user:", error);
-  });
-  
-  //search about specific user
-User.find({ email: "xyz@example2.com" })
-  .then((users) => {
-    console.log("Users found:", users);
-  })
-  .catch((error) => {
-    console.error("Error finding users", error);
-  });
 
 
-
-    //update  many fields
-    User.updateMany({ email: "xyz@example2.com"}, {$set: {password: 'updatedpassword123'}} )
-    .then((result) => {
-      console.log("Document updated successfully", result);
+//delete  a user
+User.deleteOne(
+    { email: "khan22@example2.com" }
+  )
+    .then(() => {
+      console.log("User Deleted successfully");
     })
     .catch((error) => {
-      console.error("Error updating users", error);
+      console.error("Error deleting users", error);
     });
-    
 
 module.exports = app;
