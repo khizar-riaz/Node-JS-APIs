95d4f67c8480fa2db9b7b9a555da14e649293b0c
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-26 11:40:59 +0200

    env file correction  for connecting MongoDB to API

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require('http');
-const app = require('/.app');
+const app = require('./app');
 
-app.set('port', process.env.PORT ?? 5000);
+app.set('port', process.env.PORT ?? 3000);
 
 const server = http.createServer(app);
 
-server.maxConnections('listening', () => {
-    console.log('Listening on port' + (process.env.PORT ?? 5000))
+server.on('listening', () => {
+    console.log('Listening on port:' + (process.env.PORT ?? 3000))
 })
 
-server.listen(process.env.PORT ?? 5000)
+server.listen(process.env.PORT ?? 3000)
