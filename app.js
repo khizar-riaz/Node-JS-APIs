5436c8b87dc1a79ed01e83e138107fab8dac188a
MUHAMMAD KHIZAR RIAZ <58303763+khizar7422@users.noreply.github.com>
2023-07-26 12:16:14 +0200

    Insert resource in Database

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const dbConnect = require('./db/dbConnect')
+const dbConnect = require("./db/dbConnect");
 
-dbConnect()
+const User = require("./db/userModel");
+
+dbConnect();
+
+const newUser = new User({
+  email: "xyz@example2.com",
+  password: "12345678",
+});
+
+newUser
+  .save()
+  .then(() => {
+    console.log("User Inserted Successfully");
+  })
+  .catch((error) => {
+    console.log("Error inserting user:", error);
+  });
 
 module.exports = app;
